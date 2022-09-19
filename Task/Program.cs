7cc3eb157e55ec7036c616e3fa3545fd1239d305
Konstantin Ulyanov <13shipr@mail.ru>
2022-09-19 15:42:06 +0500

    схема и решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,27 @@
 ﻿// сформировать массив строк с длиной <= 3
 
+string[] array = new string[7] { "cat", "12346", "comp", "да", ";--)", "123", "4567" };
+string[] arr = new string[array.Length];
+
+void ArrayCondition(string[] array, string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayCondition(array, arr);
+PrintArray(arr);
